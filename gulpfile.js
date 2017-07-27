000320899b972a7f9a343561dcdf7b31e6443a83
fly <fly@SHHQM0008.local>
2017-07-27 18:19:50 +0800

    *

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// 引入 gulp及组件
+var gulp = require('gulp'), //gulp基础库
+  minifycss = require('gulp-minify-css'), //css压缩
+  concat = require('gulp-concat'), //合并文件
+  uglify = require('gulp-uglify'), //js压缩
+  rename = require('gulp-rename'), //文件重命名
+  notify = require('gulp-notify'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  livereload = require('gulp-livereload'); //提示
+
+gulp.task('default', function () {
+  gulp.start('watch');
+});
+
+//css处理
+//gulp.task('css', function () {
+//  return gulp.src('./components/**/*.css') //设置css
+//    .pipe(concat('themes.css')) //合并css文件到"order_query"
+//    .pipe(gulp.dest('./assets')) //设置输出路径
+//    .pipe(rename({
+//      suffix: '.min'
+//    })) //修改文件名
+//    .pipe(minifycss()) //压缩文件
+//    .pipe(gulp.dest('./assets')) //输出文件目录
+//    .pipe(notify({
+//      message: 'css task ok'
+//    })) //提示成功
+//    .pipe(livereload());
+//});
+
+//JS处理
+//gulp.task('js', function () {
+//  return gulp.src(['./components/**/*.js']) //选择合并的JS
+//    .pipe(concat('themes.js')) //合并js
+//    .pipe(gulp.dest('./assets')) //输出
+//    .pipe(rename({
+//      suffix: '.min'
+//    })) //重命名
+//    .pipe(uglify()) //压缩
+//    .pipe(gulp.dest('./assets')) //输出 
+//    .pipe(notify({
+//      message: "js task ok"
+//    })) //提示
+//    .pipe(livereload());
+//});
+
+//sass处理
+gulp.task('sass', function () {
+  return gulp.src('./components/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('themes.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./assets'))
+    .pipe(rename({
+      suffix: '.min'
+    })) //修改文件名
+    .pipe(minifycss()) //压缩文件
+    .pipe(gulp.dest('./assets')) //输出文件目录
+    .pipe(notify({
+      message: 'sass task ok'
+    })) //提示成功
+    .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(['components/**/*.scss'], ['sass']);
+});
